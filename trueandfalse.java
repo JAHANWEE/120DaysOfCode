ece76b0d8314860ebe2a99c233051ecff8366a5d
Jaani <jahanwee2001@gmail.com>
2022-07-18 18:07:39 +0530

    fifth commit

---
+++ trueandfalse.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scn = new Scanner(System.in);
+		int t= scn.nextInt();
+		
+		while(t>0){
+		    t--;
+		    
+		    int a = scn.nextInt();
+		    int b = scn.nextInt();
+		    
+		    
+		        System.out.println(a-b);
+		    
+		}
+	}
+}
